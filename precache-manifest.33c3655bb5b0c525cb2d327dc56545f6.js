e898dd4c999b31c6abe5d11a7142c83eefc61348
andrusha52 <andrushaes@gmail.com>
2020-02-06 12:59:15 +0200

    Updates

---
+++ precache-manifest.33c3655bb5b0c525cb2d327dc56545f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8ccb4dcd73ca564a82c619bc8887e1",
+    "url": "/goit-hw3-image-finder/index.html"
+  },
+  {
+    "revision": "a1c4767480cdc731f4d2",
+    "url": "/goit-hw3-image-finder/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "e7b6ccaaf9887e2ca17d",
+    "url": "/goit-hw3-image-finder/static/css/main.47131be4.chunk.css"
+  },
+  {
+    "revision": "a1c4767480cdc731f4d2",
+    "url": "/goit-hw3-image-finder/static/js/2.15ec1ffe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-hw3-image-finder/static/js/2.15ec1ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b6ccaaf9887e2ca17d",
+    "url": "/goit-hw3-image-finder/static/js/main.ed75929f.chunk.js"
+  },
+  {
+    "revision": "444218a92c41099a6e7a",
+    "url": "/goit-hw3-image-finder/static/js/runtime-main.08de8003.js"
+  }
+]);
